dfb2d192e05b8705aca3b597fed1fbca96795e4b
Milos Adnadjevic <milos@Miloss-Air.lan>
2023-10-07 17:31:55 -0400

    sidebar done

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
 
+import { ArrowUpIcon, BellIcon, ChartBarIcon, CreditCardIcon, DocumentSearchIcon, ExternalLinkIcon, HomeIcon, MailIcon } from '@heroicons/react/solid'
+
 function Sidebar() {
   return (
-    <div className='bg-slate-800 flex-none w-14 sm:w-20 h-screen'>Sidebar</div>
+    <div className='bg-slate-800 flex-none w-14 sm:w-20 h-screen'>Sidebar
+    <div className='h-20 items-center flex'>
+        <HomeIcon width={40} className='text-gray-300 left-3 sm:left-6 fixed'/>
+    </div>
+    <div className='fixed left-3 sm:left-6 top-[100px]'>
+        <ChartBarIcon
+        width={40}
+        className='bg-gray-600 p-2 rounded-lg mb-4 text-gray-300'
+        />
+        <DocumentSearchIcon
+        width={40}
+        className='bg-gray-600 p-2 rounded-lg mb-4 text-gray-300'
+        />
+        <MailIcon
+        width={40}
+        className='bg-gray-600 p-2 rounded-lg mb-4 text-gray-300'
+        />
+        <CreditCardIcon
+        width={40}
+        className='bg-gray-600 p-2 rounded-lg mb-4 text-gray-300'
+        />
+        <BellIcon
+        width={40}
+        className='bg-gray-600 p-2 rounded-lg mb-4 text-gray-300'
+        />
+    </div>
+    <div className='fixed bottom-4 left-3 sm:left-6'>
+    <ArrowUpIcon
+        width={40}
+        className='bg-gray-600 p-2 rounded-lg mb-4 text-gray-300'
+        />
+        <ExternalLinkIcon
+        width={40}
+        className='bg-gray-600 p-2 rounded-lg mb-4 text-gray-300'
+        />
+    </div>
+    </div>
   )
 }
 
