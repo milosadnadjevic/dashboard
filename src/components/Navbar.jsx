1e3ccf9fa61565e4ba52cc0ad4aaea84e0b93453
Milos Adnadjevic <milos@Miloss-Air.lan>
2023-10-07 19:12:28 -0400

    Navbar.jsx completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,14 @@
+import { TextInput } from '@tremor/react'
+import { SearchIcon } from '@heroicons/react/outline'
 import React from 'react'
 
 function Navbar() {
   return (
-    <div>
-        <h1>Dashboard</h1>
-        <div>
-            <input type="text" placeholder='Input' />
+    <div id='top' className='relative w-full sm:flex jultify-between items-center p-2'
+    >
+        <h1 className='font-bold text-gray-300'>Dashboard</h1>
+        <div className='py-2'>
+            <TextInput icon={SearchIcon} placeholder='Search...'/>
         </div>
     </div>
   )
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -31,10 +31,12 @@ function Sidebar() {
         />
     </div>
     <div className='fixed bottom-4 left-3 sm:left-6'>
-    <ArrowUpIcon
-        width={40}
-        className='bg-gray-600 p-2 rounded-lg mb-4 text-gray-300'
-        />
+        <a href="#top">
+           <ArrowUpIcon
+            width={40}
+            className='bg-gray-600 p-2 rounded-lg mb-4 text-gray-300'
+        />  
+        </a>
         <ExternalLinkIcon
         width={40}
         className='bg-gray-600 p-2 rounded-lg mb-4 text-gray-300'
