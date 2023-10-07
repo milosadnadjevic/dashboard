adc849d80e24319d042836a655ecc3c1c1a92283
Milos Adnadjevic <milos@Miloss-Air.lan>
2023-10-07 16:59:41 -0400

    heroicons installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import './App.css'
+import Sidebar from './components/Sidebar'
 
 function App() {
   
-
   return (
-    <>
+    < main className='flex'>
      <Sidebar />
-    </>
+    <div>
+      Components
+    </div>
+    </main>
   )
 }
 
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Sidebar() {
   return (
-    <div>Sidebar</div>
+    <div className='bg-slate-800 flex-none w-14 sm:w-20 h-screen'>Sidebar</div>
   )
 }
 
