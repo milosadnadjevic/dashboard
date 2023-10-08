0900ec837ff031c249c70616de1e708708812402
Milos Adnadjevic <milos@Miloss-Air.lan>
2023-10-08 12:55:05 -0400

    AreaChart component added

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Card, Metric, Text, Flex, BadgeDelta } from '@tremor/react'
 
 const CardItem = () => {
   return (
-    <div>CardItem</div>
+    <Card className="w-xs" decoration="top" decorationColor="indigo">
+    <Flex>
+        <Text>Sales</Text>
+        <BadgeDelta deltaType='moderateIncrease'>+12.5%</BadgeDelta>      
+    </Flex>
+    <Metric>$ 34,743</Metric>
+  </Card>
   )
 }
 
--- src/components/LeftColumn.jsx
+++ src/components/LeftColumn.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import CardItem from './CardItem'
+import AreaChartComponent from './AreaChartComponent'
 
 const LeftColumn = () => {
   return (
     <div className='w-full flex flex-col justify-between'>
         <div className='flex flex-col lg:flex-row gap-2 w-full'>
-            <p>CardItem</p>
-            <p>CardItem</p>
-            <p>CardItem</p>
+            <CardItem />
+            <CardItem />
+            <CardItem />
+        </div>
+        <div className='flex-auto w-full'>
+            <AreaChartComponent />
         </div>
     </div>
   )
