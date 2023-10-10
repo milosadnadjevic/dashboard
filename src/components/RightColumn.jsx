e5f1cb09d8ba7f0c9a026eeb73ee67d399e1ac03
Milos Adnadjevic <milos@Miloss-Air.lan>
2023-10-09 21:50:26 -0400

    webanalytics component added

--- src/components/RightColumn.jsx
+++ src/components/RightColumn.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import SalesItem from './SalesItem'
+import WebAnalytics from './WebAnalytics'
 
 const RightColumn = () => {
   return (
-    <div className='w-full p-2s'>
+    <div className='w-full p-2'>
       <SalesItem />
+      <WebAnalytics />
     </div>
   )
 }
